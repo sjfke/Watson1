fae4fd77e76ddb75b5c7bf1a03a15f3d96ce8f81
Geoff Collis <gcollis@ymail.com>
2017-12-18 19:12:25 +0100

    add watson nlc

--- app/views.py
+++ app/views.py
@@ -5,58 +5,67 @@ import subprocess
 
 app = Flask(__name__)
 
-import subprocess
-
 from app import app
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
 
 @app.route('/admin')
 def hello_admin():
    return 'Hello Admin'
 
+
 @app.route('/guest/<guest>')
 def hello_guest(guest):
    return 'Hello %s as Guest' % guest
 
+
 @app.route('/user/<name>')
 def hello_user(name):
-   if name =='admin':
+   if name == 'admin':
       return redirect(url_for('hello_admin'))
    else:
-      return redirect(url_for('hello_guest', guest = name))
+      return redirect(url_for('hello_guest', guest=name))
+
 
 @app.route('/success/<name>')
 def success(name):
-   return render_template('success.html', name = name)
+   return render_template('success.html', name=name)
 
-@app.route('/login', methods = ['POST', 'GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
    if request.method == 'POST':
       user = request.form['nm']
-      return redirect(url_for('success', name = user))
+      return redirect(url_for('success', name=user))
    else:
       # user = request.args.get('nm', '')
       return render_template('login.html')
+
   
 @app.route('/hello/<user>')
 def hello_name(user):
-   return render_template('hello.html', name = user)
+   return render_template('hello.html', name=user)
+
 
 @app.route('/result')
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+   dict = {'phy':50, 'che':60, 'maths':70}
+   return render_template('result.html', result=dict)
+
 
 @app.route('/say_hello')
 def say_hello():
    return render_template('say_hello.html')
+
 
 @app.route('/date')
 def cli():
@@ -66,3 +75,30 @@ def cli():
         return "An error occurred({0}: {1}".format(e.returncode, e.output)
 
     return 'Date: %s' % cmd
+
+@app.route('/watson_nlc')
+def watson_nlc():
+    
+    import json
+    from watson_developer_cloud import NaturalLanguageUnderstandingV1
+    from watson_developer_cloud.natural_language_understanding_v1 import Features, CategoriesOptions
+
+    from ConfigParser import SafeConfigParser
+    config = SafeConfigParser()
+    config.read('config.ini')
+
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        username=config.get('watson', 'username'),
+        password=config.get('watson', 'password'),
+        version=config.get('watson', 'version')
+    )
+
+
+    response = natural_language_understanding.analyze(
+        url='https://fr.wikipedia.org/wiki/Gen%C3%A8ve',
+        features=Features(
+            categories=CategoriesOptions()
+        )
+    )
+
+    return(json.dumps(response))
